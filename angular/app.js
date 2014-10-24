b9466ba39839e94299426349e4eee920707f13e2
rpg300 <rpg300@gmail.com>
2014-10-24 17:14:41 -0400

    Got the view working
    Got the view working

--- angular/app.js
+++ angular/app.js
@@ -1,4 +1,44 @@
-var appFarkle = angular.module('appFarkle', []);
+var appFarkle = angular.module('appFarkle', ['ngRoute']);
 
-appFarkle.controller("myController", function(){
+appFarkle.controller("myController", function($scope){
+	$scope.bolSwitch = true;
+	$scope.changeRoute = function(bolSwitch){
+/*		switch(bolSwitch){
+			case true:
+					window.location.href = '#hey';
+			break;
+			case false:
+					window.location.href = '#heyyyy';
+			break;
+		}*/
+		/*window.location.href = '#' + bolSwitch;*/
+		window.location.href = '#game';
+			/*$scope.bolSwitch = !bolSwitch;*/
+	}
 });
+
+appFarkle.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {    
+    $routeProvider.when("/settings",
+    {
+		templateUrl: "angular/partials/settings.html",
+		controller: "myController"
+    })
+    .when("/game",
+    {
+        templateUrl: "angular/partials/game.html",
+        controller: "myController"
+    })
+    .when("/MainMenu",
+    {
+        templateUrl: "angular/partials/MainMenu.html",
+        controller: "myController"
+    })
+    .otherwise(
+    	{
+    		redirectTo: '/MainMenu'
+    	}
+    );
+
+     /*$locationProvider.html5Mode(true);*/
+
+}])
